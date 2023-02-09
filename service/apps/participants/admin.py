bf38be05436e92f94b1cacf87b0518ed7861c6b4
Iagocanalejas <iagocanalejas@gmail.com>
2023-02-09 18:24:14 +0100

    feat: add Penalties to admin page

--- service/apps/participants/admin.py
+++ service/apps/participants/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from apps.participants.models import Participant
+from apps.participants.models import Participant, Penalty
+
+
+class PenaltyInline(admin.TabularInline):
+    model = Penalty
 
 
 class ParticipantAdmin(admin.ModelAdmin):
+    inlines = [PenaltyInline]
     list_filter = ('club', 'race__league')
 
 
 admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Penalty, admin.ModelAdmin)
