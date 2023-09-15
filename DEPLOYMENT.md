845c3753fb64bc312531c53f8ea26fca9338f7c3
Iagocanalejas <iagocanalejas@gmail.com>
2023-09-15 13:16:21 +0200

    remove unused stuff

--- DEPLOYMENT.md
+++
@@ -1,7 +0,0 @@
-# Deployment Tasks
-
-All notable changes required when deploying a version.
-
-# [0.3.0]
-
-* After migrating load `clubs.yaml` with the new scrapped metadata
--- scripts/decrypt_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ENV_DECODE_SECRET" --output .env environment.gpg
