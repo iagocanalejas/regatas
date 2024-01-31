e84ce35446a2f6b09e21de30d8872b2335277520
Iagocanalejas <iagocanalejas@gmail.com>
2024-01-31 22:58:08 +0100

    SIMPLIFY: remove all the web related stuff

--- rest/models/flag.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Flag struct {
-	ID      int64  `json:"id"`
-	Name    string `json:"name"`
-	Edition *int   `json:"edition,omitempty"`
-}
---
+++ tests/apps/json_validators_test.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.utils import timezone
+from jsonschema.exceptions import ValidationError
+
+from apps.races.models import Flag, Race
+from apps.schemas import MetadataBuilder
+from rscraping import Datasource
+
+
+class JSONValidatorsTest(TestCase):
+    def setUp(self):
+        self.flag = Flag(name="TEST FLAG")
+        self.flag.save()
+
+    def test_race_metadata_valid_datasource(self):
+        metadata = MetadataBuilder().ref_id(1).datasource_name(Datasource.ARC).values("details_page", "test")
+        race = Race(
+            date=timezone.now(),
+            flag=self.flag,
+            flag_edition=1,
+            gender="ALL",
+            metadata={"datasource": [metadata.build()]},
+        )
+        race.save()
+
+    def test_race_metadata_invalid_datasource(self):
+        race = Race(
+            date=timezone.now(),
+            flag=self.flag,
+            flag_edition=1,
+            gender="ALL",
+            metadata={"datasource": [{"ref_id": "1", "datasource_name": "arc", "key": "details_page"}]},
+        )
+        with self.assertRaises(ValidationError):
+            race.save()
